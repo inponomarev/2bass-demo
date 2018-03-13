435aed9f2209f9cce6543681ab86dc7b0053d3c0
inponomarev <iponomarev@mail.ru>
2018-03-13 06:32:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 2bass demo
+
+Demo example for [2bass](https://github.com/CourseOrchestra/2bass).
+
+## Usage
+
+
+1. Modify bass.properties and set:
+
+** `score.path` — actual path to `score` folder
+** `jdbc.url` — actual JDBC URL to connect to your database
+** `jdbc.username`  — database username 
+** `jdbc.password`  — database password
+
+2. Change directory to `bin` subfolder of 2bass installation directory
+3. Run 
+
+```
+ bass apply <path-to-bass.properties-file>
+```
+ 
---
+++ score/orders/orders.sql
@@ -0,0 +1,38 @@
+create schema orders version '1.0';
+-- *** TABLES ***
+/**Заголовок счёта*/
+CREATE TABLE OrderHeader(
+  id VARCHAR(30) NOT NULL,
+  date DATETIME,
+  customer_id VARCHAR(30),
+  
+  /**Название клиента */
+  customer_name VARCHAR(50),
+  manager_id VARCHAR(30),
+  CONSTRAINT Pk_OrderHeader PRIMARY KEY (id)
+);
+
+/**Строка счёта*/
+CREATE TABLE OrderLine(
+  order_id VARCHAR(30) NOT NULL,
+  line_no INT NOT NULL,
+  item_id VARCHAR(30) NOT NULL,
+  item_name VARCHAR(100),
+  qty INT NOT NULL DEFAULT 0,
+  cost REAL NOT NULL DEFAULT 0.0,
+  CONSTRAINT Idx_OrderLine PRIMARY KEY (order_id, line_no)
+);
+
+
+CREATE TABLE test (
+  id INT NOT NULL IDENTITY PRIMARY KEY,
+  desc VARCHAR(10)
+);
+
+-- *** FOREIGN KEYS ***
+ALTER TABLE OrderLine ADD CONSTRAINT fk_OrderLine FOREIGN KEY (order_id) REFERENCES OrderHeader(id);
+-- *** INDICES ***
+
+-- *** VIEWS ***
+create materialized view OrderedQty as
+  select item_id, sum(qty) as qty from OrderLine group by item_id;
